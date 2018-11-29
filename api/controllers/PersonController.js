4646c1b0bd11ceb15ce828b8d789109af6c51894
Guoquan <18436617@life.hkbu.edu.hk>
2018-11-29 13:22:54 +0800

    good algorithm and Delete button

--- api/controllers/PersonController.js
+++ api/controllers/PersonController.js
@@ -20,30 +20,46 @@ module.exports = {
 
         var age = req.body.Person.age;
         var smoking = req.body.Person.smoking;
-        var asthma= req.body.Person.Asthma;
+        var asthma = req.body.Person.Asthma;
         var pad = req.body.Person.PAD;
-        var size = req.body.Person.sizeOfTheOriginalTumour;
+        var size = req.body.Person.sixMonthsMI;
         var deathRate1 = 0;
         var deathRate2 = 0;
         var deathRate3 = 0;
         var deathRate4 = 0;
+        var deathRate5 = 0.85106383;
 
-        if(smoking) deathRate1 = 0.9924;
-        else if(!smoking) deathRate1 = 0.98;
+        if (smoking) deathRate1 = 1.032;
+        else if (!smoking) deathRate1 = 0.98;
 
-        if(asthma) deathRate2 = 0.9734;
-        else if(!asthma) deathRate2 = 0.92;
+        if (asthma) deathRate2 = 1.042;
+        else if (!asthma) deathRate2 = 0.95;
 
-        if(pad) deathRate3 = 0.9822;
-        else if(!pad) deathRate3 = 0.85;
+        if (pad) deathRate3 = 1.04;
+        else if (!pad) deathRate3 = 0.983;
 
-        if(size) deathRate4 = 1;
-        else if(!size) deathRate4 = 0.867;
+        if (size) deathRate4 = 1;
+        else if (!size) deathRate4 = 0.963;
 
-        var deathRate = deathRate1*deathRate2*deathRate3*deathRate4*100;
+        // switch (1) {
+        //     case size=1:
+        //         deathRate4 = 1.001;
+        //         break;
+        //     case size=2:
+        //         deathRate4 = 1.01;
+        //         break;
+        //     case size=3:
+        //         deathRate4 = 1.05;
+        //         break;
+        //     case size=4:
+        //         deathRate4 = 1.1;
+        //         break;
+        // }
+
+        var deathRate = deathRate1 * deathRate2 * deathRate3 * deathRate4 * deathRate5 * 100;
         deathRate = deathRate.toFixed(2); // 保留两位小数
         // deathRate = Math.round(deathRate);  保留整数
-        return res.ok("i can "+ deathRate +"% guarantee you will die in 1 year");
+        return res.ok("i can " + deathRate + "% guarantee you will die in 1 year");
     },
 
     // action - index
@@ -139,7 +155,7 @@ module.exports = {
         const numOfItemsPerPage = 2;
 
         const qName = req.query.patientName || "";
-       
+
         var models = await Person.find({
             limit: numOfItemsPerPage,
             skip: numOfItemsPerPage * qPage,
